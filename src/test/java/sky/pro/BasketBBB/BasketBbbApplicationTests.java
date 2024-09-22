48676cf1b84378c759468b6c4db7d7d03bad23a8
днс <kreshvan12317@yandex.ru>
2024-09-22 15:55:00 +0300

    com

---
+++ src/test/java/sky/pro/BasketBBB/BasketBbbApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.BasketBBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketBbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
